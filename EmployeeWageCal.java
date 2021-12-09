99edf5bc5cdd4faab5190f39d3bd3a19e95a10c1
Durga Vaishnavi <durgavaishnavisb@gmail.com>
2021-12-09 15:51:38 -0800

    UPDATED the attendance in employee_wage_calculator_UC1

--- EmployeeWageCal.java
+++ EmployeeWageCal.java
@@ -2,5 +2,11 @@ public class EmployeeWageCal{
 	public static void main(String[] args)
 		{
 			System.out.println("Welcome to employeeWageCalculating problem");
+			int IS_FULL_TIME = 1;
+			double EmpCheck = Math.floor(Math.random()*10)%2;
+			if(EmpCheck == IS_FULL_TIME)
+				System.out.println("Employee is present");
+			else
+				System.out.println("Employee is absent");
 		}
 }
